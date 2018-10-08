b3bc560a3a0a388b39c57f3735ac6b069f53991e
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-10-08 16:26:35 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,6 +14,7 @@ class Transfer
   end 
   
   def execute_transaction 
+    binding.pry
     unless @@all.include?(self) || !self.valid?
       sender.balance -= amount 
       receiver.balance += amount
