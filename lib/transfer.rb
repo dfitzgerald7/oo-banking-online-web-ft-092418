e7faba385e734357867cff2ac78da7216a49ec69
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-10-08 16:38:20 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -32,6 +32,7 @@ class Transfer
     if @@all.include?(self)
       self.sender.balance += self.amount
       self.receiver.balance -= self.amount
+      self.status = "reversed"
     end 
   end 
   
