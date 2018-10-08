0cc5c875a383f01aa69bfa8d0589f370592fb34b
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-10-08 16:05:52 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,8 +1,9 @@
 class BankAccount
   
-  attr_accessor :name
+  attr_accessor :name, :balance
   
-  def initialize(name)
+  def initialize(name, balance = 1000)
     self.name = name 
+    self.balance = balance
   end 
 end
